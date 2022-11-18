f135e9185482d32dd8424d2fed1ba2946684e10b
Jim Su <jimsu@protonmail.com>
2022-11-18 10:33:30 -0500

    Add high contrast mode

---
+++ frontend/components/ThemePicker.tsx
@@ -0,0 +1,31 @@
+import { useTheme } from "next-themes";
+import { useEffect, useState } from "react";
+
+const options = ["default", "contrast"];
+
+export const ThemePicker = () => {
+  const [mounted, setMounted] = useState(false);
+  const [themeIndex, setThemeIndex] = useState<number>(0);
+  const { theme, setTheme } = useTheme();
+
+  useEffect(() => setMounted(true), []);
+
+  if (!mounted) {
+    return null;
+  }
+
+  return (
+    <button
+      type="button"
+      className="w-full text-white font-bold mt-1 block px-3 py-2 bg-brown-700 rounded-md text-sm shadow-sm focus:bg-brown-900 hover:bg-brown-800"
+      onClick={() => {
+        const newIndex = (themeIndex + 1) % options.length;
+        setThemeIndex(newIndex);
+        setTheme(options[newIndex]);
+        console.log(options[newIndex]);
+      }}
+    >
+      {options[themeIndex]}
+    </button>
+  );
+};
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -6,19 +6,20 @@ module.exports = {
     "./pages/**/*.{js,ts,jsx,tsx}",
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class",
   theme: {
     colors: {
       white: "#ffffff",
       black: "#000000",
       papyrus: {
-        100: "#ffedd5",
-        200: "#fed7aa",
-        300: "#fdba74",
+        100: "var(--papyrus-100)",
+        200: "var(--papyrus-200)",
+        300: "var(--papyrus-300)",
       },
       brown: {
-        700: "#a16207",
-        800: "#854d0e",
-        900: "#713f12",
+        700: "var(--brown-700)",
+        800: "var(--brown-800)",
+        900: "var(--brown-900)",
       },
       transparent: "rgba(0, 0, 0, 0)",
     },
