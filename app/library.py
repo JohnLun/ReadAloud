3e046a07a724d8a3219f8d1252e773e349e36ab2
hajygeld <87397964+hajygeld@users.noreply.github.com>
2022-11-20 18:33:03 -0500

    Code changes for functions.

--- app/library.py
+++ app/library.py
@@ -1,18 +1,76 @@
 """Library functions for calling in application code."""
+# pylint: skip-file 
+# flake8: noqa
+
+# type: ignore
 
 import locale
+from http.client import HTTPException
 
 from PIL import Image
+import time
 
-# flake8: noqa
-locale.setlocale(locale.LC_ALL, "C")
+from pandas import DataFrame
+from pygame import mixer
+import PyPDF2
+import docx
+import pandas as pd
+import imghdr
+
 
 from tesserocr import PyTessBaseAPI
+
 
 
 def convert_image_to_text(image: Image) -> str:
     """Extract text from a PIL Image."""
     with PyTessBaseAPI(path="./app") as api:
+        test = imghdr.what(image)
+        if test != 'png' and test != 'jpeg' and test != 'gif' and test != 'jpg':
+            raise HTTPException
         api.SetImage(image)
         text: str = api.GetUTF8Text()
         return text
+
+
+def take_in_MP3(name) -> str:
+    mixer.init()
+    # Load the name of the MP3 file that is to be subsequently played.
+    mixer.music.load(name)
+    mixer.music.play()
+    # Wait for the audio file to stop playing before exiting the function.
+    while mixer.music.get_busy():
+        time.sleep(1)
+
+def convert_PDF_to_text(pdfname) -> str:
+    # creating a pdf file object
+    pdf_File_Obj = open(pdfname + '.pdf', 'rb')
+
+    # creating a pdf reader object
+    pdf_Reader = PyPDF2.PdfFileReader(pdf_File_Obj)
+
+    # printing number of pages in pdf file
+    print(pdf_Reader.numPages)
+
+    # creating a page object
+    pageObj = pdf_Reader.getPage(0)
+
+    # extracting text from page
+    print(pageObj.extractText())
+
+    # closing the pdf file object
+    pdf_File_Obj.close()
+
+
+def convert_docx_to_plain_text(name) -> str:
+        doc = docx.Document(name + '.docx')
+        fullText = []
+        for para in doc.paragraphs:
+            fullText.append(para.text)
+        return '\n'.join(fullText)
+
+
+def convert_xlsx_to_plain_test(name) -> DataFrame:
+    df = pd.read_excel(name + '.xlsx')
+    return df
+
