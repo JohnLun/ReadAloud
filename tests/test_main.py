5ad925ffc289a63c4eb58f2b6546f974e457c83b
hajygeld <87397964+hajygeld@users.noreply.github.com>
2022-11-21 10:59:58 -0500

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -16,15 +16,15 @@ def test_root() -> None:
 
 
 def test_convert_image_to_text() -> None:
-    """Test that the image can be converted to text."""
+    """Test that the image can be converted."""
     assert (
-        app.library.convert_image_to_text(Image.open("tests/data/test.png"))
-        == "G Search with Google or enter address\n"
+            app.library.convert_image_to_text(Image.open("tests/data/test.png"))
+            == "G Search with Google or enter address\n"
     )
 
 
 def test_image() -> None:
-    """Test that the image can be converted to text."""
+    """Test that the image can be converted."""
     with open("tests/data/test.png", "rb") as f:
         assert app.main.image(
             UploadFile(filename="test.png", file=f, content_type="image/png")
@@ -32,7 +32,28 @@ def test_image() -> None:
 
 
 def test_image_failure() -> None:
-    """Test that an invalid image raises an HTTP Bad Request Exception."""
+    """Invalid image raises an HTTP Exception."""
     with open("tests/data/test.png", "rb") as f:
         with pytest.raises(HTTPException):
             app.main.image(UploadFile(filename="test.png", file=f))
+
+
+def test_tts_to_text() -> None:
+    """Checks that TTS works"""
+    assert (
+        app.library.tts_to_mp3(app.library.convert_pdf_to_text('tests/data/Document'))
+    )
+
+
+def test_convert_docx_to_plain_text() -> None:
+    """Checks PDF is converted to text."""
+    assert (
+        app.library.convert_docx_to_plain_text('tests/data/doctest')
+    )
+
+
+def test_convert_xlsx_to_plain_text() -> None:
+    """Test whether returned DataFrame is empty or not."""
+    assert (
+        app.library.convert_xlsx_to_plain_test('tests/data/ultimatetisito')
+    )
