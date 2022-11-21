f192a8d72631c629b826207309ec8c015cbe0828
hajygeld <87397964+hajygeld@users.noreply.github.com>
2022-11-21 09:40:07 -0500

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -18,8 +18,8 @@ def test_root() -> None:
 def test_convert_image_to_text() -> None:
     """Test that the image can be converted to text."""
     assert (
-        app.library.convert_image_to_text(Image.open("tests/data/test.png"))
-        == "G Search with Google or enter address\n"
+            app.library.convert_image_to_text(Image.open("tests/data/test.png"))
+            == "G Search with Google or enter address\n"
     )
 
 
@@ -37,20 +37,24 @@ def test_image_failure() -> None:
         with pytest.raises(HTTPException):
             app.main.image(UploadFile(filename="test.png", file=f))
 
+
 def test_tts_to_text() -> None:
-    """Checks that TTS works and the MP3 function returns the necessary data."""
-    assert(
-        app.library.tts_to_mp3(app.library.convert_pdf_to_text('Document'))
+    """Checks that TTS works and the MP3 
+    function returns the necessary data."""
+    assert (
+        app.library.tts_to_mp3(app.library.convert_pdf_to_text('data/Document'))
     )
+
 
 def test_convert_docx_to_plain_text() -> None:
     """Checks that PDF is actually converted to plain text."""
-    assert(
-        app.library.convert_docx_to_plain_text('doctest')
+    assert (
+        app.library.convert_docx_to_plain_text('data/doctest')
     )
+
 
 def test_convert_xlsx_to_plain_text() -> None:
     """Test whether returned DataFrame is empty or not."""
-    assert(
-        app.library.convert_xlsx_to_plain_test('ultimatetisito')
+    assert (
+        app.library.convert_xlsx_to_plain_test('data/ultimatetisito')
     )
