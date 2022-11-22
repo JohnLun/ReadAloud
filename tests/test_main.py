34907eafd41de0cc7174ef6b3bb26ff8c664887d
hajygeld <87397964+hajygeld@users.noreply.github.com>
2022-11-22 09:04:15 -0500

    Update test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -57,8 +57,9 @@ def test_convert_docx_to_plain_text() -> None:
 def test_docx() -> None:
     """Test that the docx can be converted."""
     with open("tests/data/doctest.docx", "rb") as f:
-        assert app.main.docx(UploadFile(filename="doctest.docx",
-            file=f, content_type="doctest/docx")
+        assert app.main.docx(
+            UploadFile(filename="doctest.docx",
+                       file=f, content_type="doctest/docx")
         ) == {"text": app.main.convert_docx_to_plain_text('doctest.docx')}
 
 
