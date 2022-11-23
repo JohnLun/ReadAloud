294154fe113e595a35cf7790ed164976ca524bf7
hajygeld <87397964+hajygeld@users.noreply.github.com>
2022-11-22 19:14:42 -0500

    Test

--- app/main.py
+++ app/main.py
@@ -51,7 +51,7 @@ def docx(file: UploadFile) -> dict[str, str]:
     with open(random_name + '.docx', 'wb') as f:
         f.write(contents)
         text = convert_docx_to_plain_text(random_name + '.docx')
-    os.remove(random_name + '.docx')
+    #os.remove(random_name + '.docx')
     return {"text": text, "mp3": tts_to_mp3(text)}
 
 
@@ -63,5 +63,5 @@ def pdf(file: UploadFile) -> dict[str, str]:
     with open(random_name + '.pdf', 'wb') as f:
         f.write(contents)
         text = convert_pdf_to_text(random_name + '.pdf')
-    os.remove(random_name + '.pdf')
+    #os.remove(random_name + '.pdf')
     return {"text": text, "mp3": tts_to_mp3(text)}
